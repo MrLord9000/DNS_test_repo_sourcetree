244d260dcb11e7bf4bc41a3900956e97eabff695
Filip Mazurek <f.maz.98@outlook.com>
2020-03-23 11:49:36 +0100

    Added TestScript.cs

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] private float testVariable;
+
+    void Start()
+    {
+        testVariable = 0.0f;    
+    }
+
+    void Update()
+    {
+        DoSomething();
+    }
+
+    private void DoSomething()
+    {
+        Debug.Log("Did something!");
+    }
+}
